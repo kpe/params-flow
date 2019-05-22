48402badfb4decf38b88685669f94c638ac1416b
kpe <kpe.git@gmailbox.org>
2019-05-22 09:29:52 +0200

    v0.0.10 - minor coverage and pep8 updates

--- params_flow/__init__.py
+++ params_flow/__init__.py
@@ -2,12 +2,11 @@
 #
 # created by kpe on 08.04.2019 at 8:52 PM
 #
-
 from __future__ import division, absolute_import, print_function
-
-__version__ = '0.0.8'
-
 
 from .layer import Layer
 from .model import Model
 from .normalization import LayerNormalization
+
+
+__version__ = '0.0.10'
--- tests/__init__.py
+++ tests/__init__.py
@@ -2,5 +2,4 @@
 #
 # created by kpe on 08.04.2019 at 9:05 PM
 #
-
 from __future__ import division, absolute_import, print_function
